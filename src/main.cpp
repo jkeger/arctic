5837ff8c080074ea59d70ad1d210f894ae69ca3a
Jacob Kegerreis <jacob.kegerreis@durham.ac.uk>
2021-03-11 10:21:48 +0000

    Tidy main and add custom code option

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,74 @@
 #include "traps.hpp"
 #include "util.hpp"
 
+static bool custom_mode = false;
+
+/*
+    Run arctic with --custom or -c to execute this manually-editable code.
+    
+    A good place to run your own quick tests or use arctic without any wrappers.
+    Remember to call make to recompile after editing.
+    
+    Demo version:
+        + Make a test image and save it to a txt file.
+        + Load the image from txt.
+        + Add parallel and serial CTI.
+        + Remove the CTI and save the result to file.
+*/
+int run_custom_code() {
+
+    // Write an example image to a txt file
+    save_image_to_txt(
+        (char*)"image_test_pre_cti.txt",
+        // clang-format off
+        std::valarray<std::valarray<double>>{
+            {0.0,   0.0,   0.0,   0.0},
+            {200.0, 0.0,   0.0,   0.0},
+            {0.0,   200.0, 0.0,   0.0},
+            {0.0,   0.0,   200.0, 0.0},
+            {0.0,   0.0,   0.0,   0.0},
+            {0.0,   0.0,   0.0,   0.0},
+        }  // clang-format on
+    );
+
+    // Load the image
+    std::valarray<std::valarray<double>> image_pre_cti =
+        load_image_from_txt((char*)"image_test_pre_cti.txt");
+    print_v(1, "Loaded test image from image_test_pre_cti.txt: \n");
+    print_array_2D(image_pre_cti);
+
+    // CTI model parameters
+    TrapInstantCapture trap(10.0, -1.0 / log(0.5));
+    std::valarray<Trap> traps = {trap};
+    ROE roe(1.0, true, false, true);
+    CCD ccd(1e4, 0.0, 1.0);
+    int express = 5;
+    int offset = 0;
+    int start = 0;
+    int stop = -1;
+
+    // Add parallel and serial CTI
+    std::valarray<std::valarray<double>> image_post_cti = add_cti(
+        image_pre_cti, &roe, &ccd, &traps, express, offset, start, stop, &roe, &ccd,
+        &traps, express, offset, start, stop);
+    print_v(1, "Image with CTI added: \n");
+    print_array_2D(image_post_cti);
+
+    // Remove CTI
+    int iterations = 3;
+    std::valarray<std::valarray<double>> image_remove_cti = remove_cti(
+        image_post_cti, iterations, &roe, &ccd, &traps, express, offset, start, stop,
+        &roe, &ccd, &traps, express, offset, start, stop);
+    print_v(1, "Image with CTI removed: \n");
+    print_array_2D(image_remove_cti);
+
+    // Save the final image
+    save_image_to_txt((char*)"image_test_cti_removed.txt", image_remove_cti);
+    print_v(1, "Saved final image to image_test_cti_removed.txt \n");
+
+    return 0;
+}
+
 /*
     Print help information.
 */
@@ -34,6 +102,10 @@ void print_help() {
         "        0       No printing (except errors etc). \n"
         "        1       Standard. \n"
         "        2       Extra details. \n"
+        "-c, --custom \n"
+        "    Execute the custom code in the run_custom_code() function at the very \n"
+        "    top of main.cpp. For manual editing to test or run arctic without using \n"
+        "    any wrappers. The demo version adds then removes CTI from a test image. \n"
         "\n"
         "See README.md for more information. \n");
 }
@@ -43,10 +115,11 @@ void print_help() {
 */
 void parse_parameters(int argc, char** argv) {
     // Short options
-    const char* const short_opts = ":hv:";
+    const char* const short_opts = ":hv:c";
     // Full options
     const option long_opts[] = {{"help", no_argument, nullptr, 'h'},
                                 {"verbosity", required_argument, nullptr, 'v'},
+                                {"custom", no_argument, nullptr, 'c'},
                                 {0, 0, 0, 0}};
 
     // Parse options
@@ -62,14 +135,17 @@ void parse_parameters(int argc, char** argv) {
             case 'v':
                 set_verbosity(atoi(optarg));
                 break;
+            case 'c':
+                custom_mode = true;
+                break;
             case ':':
                 printf(
-                    "Error: %s requires a value. Run with -h for help. \n",
+                    "Error: Option %s requires a value. Run with -h for help. \n",
                     argv[optind - 1]);
                 exit(1);
             case '?':
                 printf(
-                    "Error: %s not recognised. Run with -h for help. \n",
+                    "Error: Option %s not recognised. Run with -h for help. \n",
                     argv[optind - 1]);
                 exit(1);
         }
@@ -82,49 +158,32 @@ void parse_parameters(int argc, char** argv) {
 }
 
 /*
-    ...
-    
+    Main program.
+    
     Parameters
     ----------
     -h, --help
         Print help information and exit.
-    
+    
     -v <int>, --verbosity=<int>
         The verbosity parameter to control the amount of printed information:
             0       No printing (except errors etc).
             1       Standard.
             2       Extra details.
+    
+    -c, --custom
+        Execute the custom code in the run_custom_code() function at the very
+        top of this file. For manual editing to test or run arctic without using
+        any wrappers. The demo version adds then removes CTI from a test image.
 */
 int main(int argc, char** argv) {
 
     parse_parameters(argc, argv);
 
-    // Test image
-    std::valarray<std::valarray<double>> image_pre_cti, image_post_cti,
-        image_remove_cti;
-    TrapInstantCapture trap(10.0, -1.0 / log(0.5));
-    std::valarray<Trap> traps = {trap};
-    ROE roe(1.0, true, false, true);
-    CCD ccd(1e4, 0.0, 1.0);
-    int express = 5;
-    // image_pre_cti = load_image_from_txt((char*) "dev/hst_acs_jc0a01h8q_raw.txt");
-    image_pre_cti = load_image_from_txt((char*)"dev/hst_acs_10_col.txt");
-    // image_pre_cti = {
-    //     // clang-format off
-    //     {0.0,   0.0,   0.0,   0.0},
-    //     {200.0, 0.0,   0.0,   0.0},
-    //     {0.0,   200.0, 0.0,   0.0},
-    //     {0.0,   0.0,   200.0, 0.0},
-    //     {0.0,   0.0,   0.0,   0.0},
-    //     {0.0,   0.0,   0.0,   0.0}
-    //     // clang-format on
-    // };
-    // print_array_2D(image_pre_cti);
-    image_post_cti = add_cti(image_pre_cti, &roe, &ccd, &traps, express);
-    // print_array_2D(image_post_cti);
-    // image_remove_cti = remove_cti(image_post_cti, 3, &roe, &ccd, &traps, express);
-    // print_array_2D(image_remove_cti);
-    save_image_to_txt((char*)"dev/image_out.txt", image_post_cti);
+    if (custom_mode) {
+        print_v(1, "# ArCTIC: Running custom code! \n\n");
+        return run_custom_code();
+    }
 
     return 0;
 }
