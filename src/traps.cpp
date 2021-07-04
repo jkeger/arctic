2cc2d141c617932be63a9a5346d4f9e4c7d0f37f
Jacob <jacob.kegerreis@durham.ac.uk>
2021-07-04 20:50:28 +0100

    Implement fill fraction and time elapsed conversions for scc

--- src/traps.cpp
+++ src/traps.cpp
@@ -475,6 +475,127 @@ TrapSlowCaptureContinuum::TrapSlowCaptureContinuum(
 }
 
 /*
+    Same as TrapContinuum
+*/
+struct TrSCCo_ff_from_te_params {
+    double time_elapsed;
+    double mu;
+    double sigma;
+};
+
+double TrSCCo_ff_from_te_integrand(double tau, void* params) {
+    struct TrSCCo_ff_from_te_params* p = (struct TrSCCo_ff_from_te_params*)params;
+
+    // Distribution density
+    double n = exp(-pow(log(tau) - log(p->mu), 2.0) / (2.0 * p->sigma * p->sigma)) /
+               (tau * p->sigma * sqrt(2.0 * M_PI));
+
+    // To integrate: n(tau) * fill_frac(tau) dtau
+    return n * exp(-p->time_elapsed / tau);
+}
+
+double TrapSlowCaptureContinuum::fill_fraction_from_time_elapsed(
+    double time_elapsed, gsl_integration_workspace* workspace) {
+    // Completely full or empty, or unset watermark
+    if (time_elapsed == 0.0)
+        return 1.0;
+    else if (time_elapsed >= std::numeric_limits<double>::max())
+        return 0.0;
+    else if (time_elapsed == -1.0)
+        return 0.0;
+
+    // Prep the integration
+    double result, error;
+    const double min = 0.0;
+    const double epsabs = 0.0;
+    const double epsrel = 1e-6;
+    const int limit = 100;
+    if (!workspace) {
+        workspace = gsl_integration_workspace_alloc(limit);
+    }
+    struct TrSCCo_ff_from_te_params params = {
+        time_elapsed, release_timescale, release_timescale_sigma};
+    gsl_function F;
+    F.function = &TrSCCo_ff_from_te_integrand;
+    F.params = &params;
+
+    // Integrate F.function from min to +infinity
+    int status = gsl_integration_qagiu(
+        &F, min, epsabs, epsrel, limit, workspace, &result, &error);
+
+    if (status) error("Integration failed, status %d", status);
+
+    return result;
+}
+
+/*
+    Same as TrapContinuum
+*/
+struct TrSCCo_te_from_ff_params {
+    TrapSlowCaptureContinuum* trap;
+    double fill_fraction;
+    gsl_integration_workspace* workspace;
+};
+
+double TrSCCo_te_from_ff_root_function(double time_elapsed, void* params) {
+    struct TrSCCo_te_from_ff_params* p = (struct TrSCCo_te_from_ff_params*)params;
+
+    // To find time such that: fill_fraction(time) - fill_fraction = 0
+    return p->trap->fill_fraction_from_time_elapsed(time_elapsed, p->workspace) -
+           p->fill_fraction;
+}
+
+double TrapSlowCaptureContinuum::time_elapsed_from_fill_fraction(
+    double fill_fraction, double time_max, gsl_integration_workspace* workspace) {
+    // Completely full or empty, or unset watermark
+    if (fill_fraction == 1.0)
+        return 0.0;
+    else if (fill_fraction == 0.0)
+        return std::numeric_limits<double>::max();
+    else if (fill_fraction == -1.0)
+        return 0.0;
+
+    // Prep for the integration
+    if (!workspace) {
+        const int limit = 100;
+        workspace = gsl_integration_workspace_alloc(limit);
+    }
+
+    // Prep the root finder
+    double root;
+    int status;
+    const int max_iter = 100;
+    const double epsabs = 0.0;
+    const double epsrel = 1e-6;
+    const gsl_root_fsolver_type* T;
+    gsl_root_fsolver* s;
+    T = gsl_root_fsolver_brent;
+    s = gsl_root_fsolver_alloc(T);
+    struct TrSCCo_te_from_ff_params params = {this, fill_fraction, workspace};
+    gsl_function F;
+    F.function = &TrSCCo_te_from_ff_root_function;
+    F.params = &params;
+
+    // Bounding values
+    double x_lo = 0.0;
+    double x_hi = time_max;
+
+    // Iterate the root finder
+    int iter = 0;
+    gsl_root_fsolver_set(s, &F, x_lo, x_hi);
+    do {
+        iter++;
+        status = gsl_root_fsolver_iterate(s);
+        root = gsl_root_fsolver_root(s);
+        x_lo = gsl_root_fsolver_x_lower(s);
+        x_hi = gsl_root_fsolver_x_upper(s);
+        status = gsl_root_test_interval(x_lo, x_hi, epsabs, epsrel);
+    } while (status == GSL_CONTINUE && iter < max_iter);
+
+    return root;
+}
+
+/*
     Calculate the fraction of filled traps after slow-capture (and release).
 
     Found by integrating the trap density distribution multiplied by the final
--- test/test_traps.cpp
+++ test/test_traps.cpp
@@ -211,10 +211,10 @@ TEST_CASE("Test continuum traps", "[traps]") {
     }
 }
 
-TEST_CASE("Test slow-capture continuum traps asdf", "[traps]") {
+TEST_CASE("Test slow-capture continuum traps", "[traps]") {
     // Narrow and wide distributions, and fast and slow capture
     TrapSlowCaptureContinuum trap_1(10.0, -1.0 / log(0.5), 0.05, 0.1);
-    TrapSlowCaptureContinuum trap_2(8.0, -1.0 / log(0.2), 0.5, 1.0);
+    TrapSlowCaptureContinuum trap_2(8.0, -1.0 / log(0.5), 0.5, 1.0);
     // Close to single-lifetime and instant-capture
     TrapSlowCaptureContinuum trap_3(10.0, -1.0 / log(0.5), 0.01, 0.01);
 
@@ -227,11 +227,63 @@ TEST_CASE("Test slow-capture continuum traps asdf", "[traps]") {
         REQUIRE(trap_1.capture_rate == 10.0);
 
         REQUIRE(trap_2.density == 8.0);
-        REQUIRE(trap_2.release_timescale == -1.0 / log(0.2));
-        REQUIRE(trap_2.release_rate == -log(0.2));
+        REQUIRE(trap_2.release_timescale == -1.0 / log(0.5));
+        REQUIRE(trap_2.release_rate == -log(0.5));
         REQUIRE(trap_2.release_timescale_sigma == 0.5);
         REQUIRE(trap_2.capture_timescale == 1.0);
         REQUIRE(trap_2.capture_rate == 1.0);
+    }
+
+    SECTION("Fill fraction from time elapsed") {
+        // Similar(ish) to a single release time
+        REQUIRE(
+            trap_1.fill_fraction_from_time_elapsed(1.0) == Approx(0.5).epsilon(0.01));
+        REQUIRE(
+            trap_2.fill_fraction_from_time_elapsed(1.0) == Approx(0.5).epsilon(0.05));
+
+        REQUIRE(
+            trap_1.fill_fraction_from_time_elapsed(2.0) == Approx(0.25).epsilon(0.01));
+        REQUIRE(
+            trap_2.fill_fraction_from_time_elapsed(2.0) == Approx(0.25).epsilon(0.2));
+
+        // Full and empty
+        REQUIRE(trap_1.fill_fraction_from_time_elapsed(0.0) == Approx(1.0));
+        REQUIRE(
+            trap_1.fill_fraction_from_time_elapsed(
+                std::numeric_limits<double>::max()) == Approx(0.0));
+    }
+
+    SECTION("Time elapsed from fill fraction") {
+        double time_max = 999;
+
+        // Similar(ish) to a single release time
+        REQUIRE(
+            trap_1.time_elapsed_from_fill_fraction(0.5, time_max) ==
+            Approx(1.0).epsilon(0.01));
+        REQUIRE(
+            trap_2.time_elapsed_from_fill_fraction(0.5, time_max) ==
+            Approx(1.0).epsilon(0.1));
+
+        REQUIRE(
+            trap_1.time_elapsed_from_fill_fraction(0.25, time_max) ==
+            Approx(2.0).epsilon(0.01));
+        REQUIRE(
+            trap_2.time_elapsed_from_fill_fraction(0.25, time_max) ==
+            Approx(2.0).epsilon(0.25));
+
+        // Full and empty
+        REQUIRE(trap_1.time_elapsed_from_fill_fraction(1.0, time_max) == Approx(0.0));
+        REQUIRE(
+            trap_1.time_elapsed_from_fill_fraction(0.0, time_max) >=
+            std::numeric_limits<double>::max());
+
+        // Convert and back
+        REQUIRE(
+            1.234 == Approx(trap_1.time_elapsed_from_fill_fraction(
+                         trap_1.fill_fraction_from_time_elapsed(1.234), time_max)));
+        REQUIRE(
+            2.468 == Approx(trap_2.time_elapsed_from_fill_fraction(
+                         trap_2.fill_fraction_from_time_elapsed(2.468), time_max)));
     }
 
     SECTION("Fill fraction from slow capture") {
