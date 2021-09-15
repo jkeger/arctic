ed44517335d9877e2d18e1a992285151dde7746c
Jacob <jacob.kegerreis@durham.ac.uk>
2021-09-15 15:41:18 +0100

    Add python base trap class to help AutoCTI wrapper

--- arcticpy/src/traps.py
+++ arcticpy/src/traps.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 
-class TrapInstantCapture(object):
+class AbstractTrap(object):
+    def __init__(self, density=1.0, release_timescale=1.0):
+        self.density = density
+        self.release_timescale = release_timescale
+
+
+class TrapInstantCapture(AbstractTrap):
     def __init__(
         self,
         density=1.0,
@@ -9,20 +15,20 @@ class TrapInstantCapture(object):
         fractional_volume_none_exposed=0.0,
         fractional_volume_full_exposed=0.0,
     ):
-        self.density = density
-        self.release_timescale = release_timescale
+        super().__init__(density, release_timescale)
+
         self.fractional_volume_none_exposed = fractional_volume_none_exposed
         self.fractional_volume_full_exposed = fractional_volume_full_exposed
 
 
-class TrapSlowCapture(TrapInstantCapture):
+class TrapSlowCapture(AbstractTrap):
     def __init__(self, density=1.0, release_timescale=1.0, capture_timescale=0.0):
         super().__init__(density, release_timescale)
 
         self.capture_timescale = capture_timescale
 
 
-class TrapInstantCaptureContinuum(TrapInstantCapture):
+class TrapInstantCaptureContinuum(AbstractTrap):
     def __init__(self, density=1.0, release_timescale=1.0, release_timescale_sigma=0.0):
         super().__init__(density, release_timescale)
 
