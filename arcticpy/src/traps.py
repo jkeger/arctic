bf23e38ceba84be4b3e74b8d743c1e886279ec8d
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-03-29 15:39:40 +0100

    added delta_ellipcity to TrapInstantCapture

--- arcticpy/src/traps.py
+++ arcticpy/src/traps.py
@@ -20,6 +20,28 @@ class TrapInstantCapture(AbstractTrap):
         self.fractional_volume_none_exposed = fractional_volume_none_exposed
         self.fractional_volume_full_exposed = fractional_volume_full_exposed
 
+    @property
+    def delta_ellipticity(self):
+
+        a = 0.05333
+        d_a = -0.03357
+        d_p = 1.628
+        d_w = 0.2951
+        g_a = 0.09901
+        g_p = 0.4553
+        g_w = 0.4132
+
+        return 4.0 * self.density * (
+            a
+            + d_a * (np.arctan((np.log10(self.release_timescale) - d_p) / d_w))
+            + (
+                g_a
+                * np.exp(
+                    -((np.log10(self.release_timescale) - g_p) ** 2.0) / (2 * g_w ** 2.0)
+                )
+            )
+        )
+
 
 class TrapSlowCapture(AbstractTrap):
     def __init__(self, density=1.0, release_timescale=1.0, capture_timescale=0.0):
