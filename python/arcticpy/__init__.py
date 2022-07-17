bb73de7e2d606eab3ef2bfb9933af0e7c84ef0cf
Jascha S <jascha.schewtschenko@port.ac.uk>
2022-07-17 13:04:17 +0100

    added missing init file

---
+++ python/arcticpy/__init__.py
@@ -0,0 +1,17 @@
+from .cti import add_cti, remove_cti, CTI_model_for_HST_ACS
+from .pixel_bounce import PixelBounce, add_pixel_bounce, remove_pixel_bounce
+from .ccd import CCDPhase, CCD
+from .roe import ROE, ROEChargeInjection, ROETrapPumping
+from .traps import (
+    TrapInstantCapture,
+    TrapSlowCapture,
+    TrapInstantCaptureContinuum,
+    TrapSlowCaptureContinuum,
+)
+try:
+    from .src.wrapper import (
+        cy_print_array as print_array,
+        cy_print_array_2D as print_array_2D,
+    )
+except ModuleNotFoundError:
+    pass
