ba33732564e17606b152e1949e90e58d8a3d8596
rjmassey <r.j.massey@durham.ac.uk>
2022-07-17 15:02:06 +0100

    Default homebrew directory changed for install on macs

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ dir_wrapper_src = os.path.join(dir_wrapper, "src")
 dir_wrapper_include = os.path.join(dir_wrapper, "include")
 dir_include = "include/"
 dir_src = "src/"
-dir_gsl = os.environ.get("DIR_GSL", '/usr/include/')
-dir_include_gsl = os.path.join(dir_gsl, "include")
-dir_lib_gsl = os.path.join(dir_gsl, "lib")
+dir_gsl = os.environ.get("DIR_GSL", '/usr/local/')
+dir_gsl_include = os.path.join(dir_gsl, "include")
+dir_gsl_lib = os.path.join(dir_gsl, "lib")
 
 # Clean (really needed anymore?)
 for root, dirs, files in os.walk(dir_wrapper, topdown=False):
@@ -44,8 +44,8 @@ extensions = [
         sources=[os.path.join(dir_wrapper_src, "wrapper.pyx"), os.path.join(dir_wrapper_src, "interface.cpp"), *ext_sources],
         language="c++",
         libraries=["gsl"],
-        runtime_library_dirs=[dir_lib_gsl],
-        include_dirs=[dir_wrapper_include, dir_include, np.get_include(), dir_include_gsl],
+        runtime_library_dirs=[dir_gsl_lib],
+        include_dirs=[dir_wrapper_include, dir_include, np.get_include(), dir_gsl_include],
         extra_compile_args=["-std=c++17", "-O3"],
         define_macros=[('NPY_NO_DEPRECATED_API', 0)],
     ),
